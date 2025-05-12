4dd2473857bc15fcb5aaac11f86543f6427cff8d
AntonioPalmaDev <antoniopalma18@outlook.com>
2025-05-11 21:30:12 -0300

    Ajuste de UX

--- js/products.js
+++ js/products.js
@@ -1,4 +1,8 @@
 const products = [
   { id: 1, name: "Camisa Roxa", price: 59.99, image: "img/camisabg.png" },
   { id: 2, name: "Tênis Branco", price: 199.90, image: "img/tenisbg.png" },
+  { id: 3, name: "Tênis Branco", price: 199.90, image: "img/tenisbg.png" },
+  { id: 4, name: "Tênis Branco", price: 199.90, image: "img/tenisbg.png" },
+  { id: 5, name: "Tênis Branco", price: 199.90, image: "img/tenisbg.png" },
+  { id: 6, name: "Tênis Branco", price: 199.90, image: "img/tenisbg.png" },
 ];
